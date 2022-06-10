36021aa2432deaf73fa0c685744eef64b1042063
md-siam <md.siam03@gmail.com>
2022-06-10 18:48:51 +0600

    first commit

--- lib/unit_testing/unit_test1.dart
+++ lib/unit_testing/unit_test1.dart
@@ -7,7 +7,9 @@ class MyUnitTest extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Unit Testing')),
-      body: Center(),
+      body: const Center(
+        child: Text('Testing'),
+      ),
     );
   }
 }
